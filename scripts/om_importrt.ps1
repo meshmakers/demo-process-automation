e0f1afbd1c3b09fd787a53a063a9c0e620406970
Gerald Lochner <gerald.lochner@meshmakers.io>
2025-10-05 18:07:41 +0200

    AB#2645: New: Added new nodes

--- scripts/om_importrt.ps1
+++ scripts/om_importrt.ps1
@@ -10,8 +10,11 @@ octo-cli -c ImportRt -f ./../data/_pipelines/upload_accounting_document_v1.yaml 
 octo-cli -c ImportRt -f ./../data/_pipelines/upload_accounting_document_v2.yaml -w
 octo-cli -c ImportRt -f ./../data/_pipelines/upload_accounting_document_v3.yaml -w
 octo-cli -c ImportRt -f ./../data/_pipelines/detect_anomalies_amount_percent_change.yaml -w
+octo-cli -c ImportRt -f ./../data/_pipelines/detect_anomalies_amount_spike_estimation.yaml -w
 
 # Import queries
 octo-cli -c ImportRt -f ./../data/_queries/_trees.yaml
+octo-cli -c ImportRt -f ./../data/_queries/_accounting_documents_all.yaml
+octo-cli -c ImportRt -f ./../data/_queries/_accounting_documents_review.yaml
 
 
