4803334fb48c3d9e9c9c2a21ee6fc028fc932ed6
Gerald Lochner <gerald.lochner@meshmakers.io>
2025-10-06 21:54:53 +0200

    AB#2706: Fixed: Added script to upload a specific file for v3 upload node.

---
+++ scripts/uploadFilev3.ps1
@@ -0,0 +1,29 @@
+param (
+    [string]$file,
+    [string]$tenant = "processautomationdemo",
+    [string]$baseUrl = "https://localhost:5020"
+)
+
+# check if file parameter is provided
+if (-not $file) {
+    Write-Host "Please provide a file path as a parameter."
+    exit 1
+}
+
+if (-not (Test-Path -Path $file)) {
+    Write-Host "File not found: $file"
+    exit 1
+}
+
+# Make full path
+$file = (Resolve-Path $file).Path
+Write-Host "Uploading file: $file"
+
+$pdfBytes = [System.IO.File]::ReadAllBytes($file)
+$response = Invoke-WebRequest -Uri "$baseUrl/$tenant/uploadaccountingdocumentv3" `
+    -Method Post `
+    -Body $pdfBytes `
+    -ContentType "application/pdf"
+
+Write-Host $response.StatusCode
+Write-Host $response.Content
